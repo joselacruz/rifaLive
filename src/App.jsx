6108d44f8738a09fd28e4d8f837c7bbe0a037b67
joselacruz <joselacruzperez@gmail.com>
2024-02-19 16:51:50 -0400

    failure imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { useRef, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import { AppBar, Container, Typography, ThemeProvider } from '@mui/material';
-
 import RaffleStatus from './components/RaffleStatus';
 import RaffleVideo from './components/RaffleVideo';
 import { createTheme } from '@mui/material/styles';
